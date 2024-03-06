6e6f3086cad4954d56313ded1e8c958cabfede67
ArranTClark <arran.clark.22@ucl.ac.uk>
2024-03-06 18:36:46 +0000

    changes to charity.js

--- server/routers/charity.js
+++ server/routers/charity.js
@@ -5,6 +5,16 @@ const prisma = new PrismaClient();
 const multer = require("multer");
 const path = require("path");
 const authJWT = require("../middleware/JWTVerification");
+const aws = require('aws-sdk');
+const multerS3 = require('multer-s3');
+
+
+// Configure AWS SDK
+aws.config.update({
+  accessKeyId: process.env.AWS_KEY_NAME,
+  secretAccessKey: process.env.AWS_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -20,25 +30,61 @@ const storage = multer.diskStorage({
     cb(null, newFilename);
   },
 });
-const upload = multer({ storage: storage });
-router.post(
-  "/upload-charity-avatar/",
-  upload.single("avatar"),
-  async (req, res) => {
-    try {
-      const file = req.file;
-      const projectId = req.query.projectId;
 
-      if (!file) {
-        return res.status(400).json({ error: "No file uploaded." });
-      }
-      res.status(200).json({ message: "File uploaded successfully." });
-    } catch (error) {
-      console.error("Failed to upload image:", error);
-      res.status(500).json({ error: "Failed to upload image" });
+// Create an S3 instance
+const s3 = new aws.S3();
+
+// Configure multer-s3 to upload files directly to S3
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'philanthrify-image-testeroonie',
+    acl: 'public-read', // Adjust the ACL as needed
+    key: function (req, file, cb) {
+      const projectId = req.query.projectId;
+      const fileName = `project_${projectId}/${Date.now()}_${file.originalname}`;
+      cb(null, fileName);
+    },
+  }),
+});
+
+router.post('/upload-charity-avatar/', upload.single('avatar'), async (req, res) => {
+  try {
+    const file = req.file;
+
+    if (!file) {
+      return res.status(400).json({ error: 'No file uploaded.' });
     }
+
+    // Access the S3 URL of the uploaded file
+    const s3Url = file.location;
+
+    res.status(200).json({ message: 'File uploaded successfully.', s3Url });
+  } catch (error) {
+    console.error('Failed to upload image:', error);
+    res.status(500).json({ error: 'Failed to upload image' });
   }
-);
+});
+
+//const upload = multer({ storage: storage });
+//router.post(
+//  "/upload-charity-avatar/",
+//  upload.single("avatar"),
+//  async (req, res) => {
+//    try {
+//      const file = req.file;
+//      const projectId = req.query.projectId;
+//
+//      if (!file) {
+//        return res.status(400).json({ error: "No file uploaded." });
+//      }
+//      res.status(200).json({ message: "File uploaded successfully." });
+//    } catch (error) {
+//      console.error("Failed to upload image:", error);
+//      res.status(500).json({ error: "Failed to upload image" });
+//    }
+//  }
+//);
 // get information on charity, teammates from charity Id
 // twice nested query to get only needed information, including only what is needed
 
